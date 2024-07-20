ec44a32a493211579cd8eae5001d52f408a5cd0e
Adrin <137534762+Adrianj83@users.noreply.github.com>
2024-07-20 19:04:46 +0330

    Update script.js

--- script.js
+++ script.js
@@ -207,3 +207,4 @@ document.addEventListener('DOMContentLoaded', function() {
      item.style.animationDelay = `${index * 0.1}s`;
      item.classList.add('animate__animated', 'animate__fadeInUp');
    });
+AOS.init(); 
